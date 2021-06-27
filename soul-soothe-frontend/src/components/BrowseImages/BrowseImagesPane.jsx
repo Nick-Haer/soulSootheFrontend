24bdbc833dbc039da6eeee6623e97fd9d75f81c9
Nick.Haer <nhaer123@gmail.com>
2021-06-26 22:12:27 -0500

    add react-router and landing page/browse page intiial

---
+++ soul-soothe-frontend/src/components/BrowseImages/BrowseImagesPane.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core'
+
+
+const BrowseImagesPane = () => {
+    return (
+        <>
+            <Typography variant="h6">
+                Browse Images Here
+            </Typography>
+        </>
+    )
+}
+
+export default BrowseImagesPane;
+
+// BrowseImagesPane.PropTypes = {
+
+// };
---
+++ soul-soothe-frontend/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core'
+
+
+const LandingPage = () => {
+    return (
+        <>
+            <Typography variant="h6">
+                Browse Images Here
+            </Typography>
+        </>
+    )
+}
+
+export default LandingPage;
